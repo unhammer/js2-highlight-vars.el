951151315ab390ce874be14a45ec3a90a2c3b50e
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-09-12 12:03:48 +0200

    version, url, packge-requires for elpa

--- js2-highlight-vars.el
+++ js2-highlight-vars.el
@@ -1,8 +1,26 @@
 ;;; js2-highlight-vars.el --- highlight occurrences of the variable under cursor
 
 ;; Copyright (C) 2009  Free Software Foundation, Inc.
-
 ;; Author:  Mihai Bazon <mihai.bazon@gmail.com>
+;; Version: 0.1
+;; URL: http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode/js2-highlight-vars-mode
+;; Package-Requires: ((js2-mode "20150909"))
+
+;;; Commentary:
+;;
+;; This is a minor mode on top of js2-mode which highlights all
+;; occurrences of the variable under the cursor within its defining
+;; scope.
+
+;;; Installation:
+;;
+;; Install this package from MELPA using `M-x install-package` and put
+;; the following in your ~/.emacs.d/init.el:
+;; (eval-after-load "js2-highlight-vars-autoloads"
+;;   '(add-hook 'js2-mode-hook (lambda () (js2-highlight-vars-mode))))
+;;
+;; If you aren't already using MELPA, see:
+;; http://melpa.milkbox.net/#/getting-started
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -16,6 +34,8 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
 
 (require 'js2-mode)
 
